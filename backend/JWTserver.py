6b2adee0d029d608791ed57989aa83dbfa920bfb
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-04-14 23:16:17 +0800

    Refactor login and registration endpoints in JWTserver.py and models.py to improve response handling and consistency

--- backend/JWTserver.py
+++ backend/JWTserver.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from models import LoginPayLoad, LoginResponseModel, LoginErrorModel\
-, AuthenticatePayLoad\
+from models import LoginPayLoad, LoginResponseModel\
+,AuthenticatePayLoad, AuthenticateResponseModel\
 ,auth_responses, login_responses
 
 app = FastAPI()
@@ -9,10 +9,10 @@ app = FastAPI()
 def main():
     return {"message":"This is the root"}
 
-@app.post("/api/v1/login", status_code = 200, response_model=LoginResponseModel, responses=login_responses)
-async def login(payload: LoginPayLoad):
+@app.post("/api/v1/login", status_code = 200, response_model = LoginResponseModel, responses = login_responses)
+async def login_user(payload: LoginPayLoad):
     pass
 
-@app.post("/api/v1/auth", status_code=201, responses=auth_responses)
-async def authenticate(payload: AuthenticatePayLoad):
+@app.post("/api/v1/register", status_code = 201, response_model = AuthenticateResponseModel, responses = auth_responses)
+async def register_user(payload: AuthenticatePayLoad):
     pass
--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,16 @@
 from pydantic import BaseModel
 
+def error_parser(model, description, example):
+    return {
+        "description":description, 
+        "content":{
+            "application/json": {
+                "schema": model.model_json_schema(),
+                "example": {"detail":example}
+                }
+            }
+        }  
+
 #payloads
 class LoginPayLoad(BaseModel):
     pass
@@ -12,26 +23,19 @@ class LoginResponseModel(BaseModel):
     detail: str
     token: str
     
-class LoginErrorModel(BaseModel):
-    detail: str
     
-def error_parser(model, description, example):
-    return {
-        "description":description, 
-        "content":{
-            "application/json": {
-                "schema": model.model_json_schema(),
-                "example": {"detail":example}
-                }
-            }
-        }  
+class AuthenticateResponseModel(BaseModel):
+    detail: str
+    token: str
+ 
+class GeneralErrorModel(BaseModel):
+    detail: str
     
 #responses
 auth_responses = {
-    409: {"detail":"user already in database"}
+    409: error_parser(GeneralErrorModel, "Authentication Error", "User is in database already")
     }
 
 login_responses = {
-    401: error_parser(LoginErrorModel, "Invalid credentials", "invalid username or password")
+    401: error_parser(GeneralErrorModel, "Invalid credentials", "invalid username or password")
     }
-
