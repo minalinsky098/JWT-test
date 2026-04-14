9aaaf84b6c082423dc8380517e52a9758860468f
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-04-14 22:13:51 +0800

    Add status codes and responses to login and authenticate endpoints in JWTserver.py

--- backend/JWTserver.py
+++ backend/JWTserver.py
@@ -8,10 +8,10 @@ app = FastAPI()
 def main():
     return {"message":"This is the root"}
 
-@app.post("/api/v1/login", responses=login_responses)
+@app.post("/api/v1/login", status_code = 200, responses=login_responses)
 async def login(payload: LoginPayLoad):
     pass
 
-@app.post("/api/v1/auth")
-async def authenticate(payload: AuthenticatePayLoad, response=auth_responses):
+@app.post("/api/v1/auth", status_code=201, responses=auth_responses)
+async def authenticate(payload: AuthenticatePayLoad):
     pass
