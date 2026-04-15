619a1132ccde2963d67643f169c775d6b31431ba
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-04-15 10:56:28 +0800

    Implement database connection pooling and fetch users from the database

--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,23 @@
 from dotenv import load_dotenv
+import os
+import asyncio
+import asyncpg
 
 load_dotenv()
+DATABASEURL = os.getenv("DATABASE_URL")
+
+def get_connection_pool(func):
+    async def wrapper(*args, **kwargs):
+        async with asyncpg.create_pool(DATABASEURL) as pool:
+            async with pool.acquire() as conn:
+                return await func(conn, *args, **kwargs)
+    return wrapper
+
+@get_connection_pool
+async def main(conn):
+    rows = await conn.fetch("SELECT * FROM users")
+    print(rows)
+    print("Connected successfully!")
+
+asyncio.run(main())
+
