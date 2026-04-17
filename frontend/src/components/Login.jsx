7210f03b93e84ea5791da1e56128756c215f6a87
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-04-17 15:10:41 +0800

    feat: add Login component and update App to include it

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,14 @@
+export default function Login (){
+    return(
+        <>
+        <h1>LOGIN</h1>
+        <form action="">
+        <input placeholder='Royce' id='firstName-el'></input>
+        <input placeholder='Albolote' id='lastName-el'></input>
+        <input placeholder='email' id='emailName-el'></input>
+        <input placeholder='password' id='password-el'></input>
+        <label></label>
+        </form>
+        </>
+    )
+}
