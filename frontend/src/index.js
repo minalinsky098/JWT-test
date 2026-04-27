d310d02013a6866e84e55cc22c8888c2a986ff89
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-04-27 19:14:49 +0800

    feat: add first and last name fields to login form and implement registration toggle functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,55 @@
+const BASE_URL = "http://127.0.0.1:8000";
+const elements = {
+    form: null,
+    emailInput: null,
+    passwordInput: null,
+    firstNameGroup: null,
+    lastNameGroup: null,
+    registerButton: null,
+    submitButton: null
+}
+
+let register = true;
+
 function main(){
-    const form = document.querySelector("form");
-    const emailInput = document.querySelector("#email");
-    const passwordInput = document.querySelector("#password")
-    form.addEventListener("submit", function(e){onSubmit(e, emailInput, passwordInput)})
+    elements.form = document.querySelector("form");
+    elements.emailInput = document.querySelector("#email");
+    elements.passwordInput = document.querySelector("#password");
+    elements.registerButton = document.querySelector("#register");
+    elements.firstNameGroup = document.querySelector("#firstName-group");
+    elements.lastNameGroup = document.querySelector("#lastName-group");
+    elements.submitButton = document.querySelector("#submit");
+
+    elements.form.addEventListener("submit", onSubmit);
+    elements.registerButton.addEventListener("click", switchRegister);
     console.log("Hello world");
 }
 
+async function switchRegister(event){
+    const {firstNameGroup, lastNameGroup, submitButton, registerButton} = elements;
+    console.log("switch", register);
+    if (register){
+        firstNameGroup.style.display = "flex";
+        lastNameGroup.style.display = "flex";
+        submitButton.textContent = "Register";
+        registerButton.textContent = "Have an account?"
+    }
+    else{
+        firstNameGroup.style.display = "none";
+        lastNameGroup.style.display = "none";
+        submitButton.textContent = "Login";
+        registerButton.textContent = "Don't have an account?"
+    }
+    register = !register;
+}
 
-
-
-function onSubmit(event, email, password){
+async function onSubmit(event){
     event.preventDefault();
+    const {emailInput, passwordInput} = elements
     console.log(email.value, password.value);
+    res = await fetch(BASE_URL);
+    res = await res.json();
+    console.log(res);
 }
 
 main()
