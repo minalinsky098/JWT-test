25d917d7be08bc65fdff11325e1111945becae39
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-06-12 13:29:31 +0800

    feat: added testing

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,1 +1,9 @@
 import pytest
+import backend.auth as auth
+
+async def test_hash_password():
+    hashed = await auth.hash_password("mypassword")
+    correct_result = await auth.check_password("mypassword", hashed)
+    error_result = await auth.check_password("wrongpassword", hashed)
+    assert correct_result == True 
+    assert error_result == False #now whcih directory do i run this, tests? or backend> or the parent JWT test
