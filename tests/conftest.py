ddd2e9884ff7ea05f47991cdc6d4cf6dfdd40283
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-06-14 14:02:48 +0800

    fix: added a scope to fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,11 @@ from backend.JWTserver import app
 from backend.auth import generate_jwt
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def client():
     with TestClient(app) as c:
         yield c
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def token():
     return generate_jwt("f7bab17f-e834-4ee8-89ca-50638dbfd705")
