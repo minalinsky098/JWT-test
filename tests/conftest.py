38c7cdc838322408d709d406def46e06da0caf2a
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-06-16 10:59:11 +0800

    feat: added a cleanup after getting a header

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,3 +13,19 @@ def client():
 @pytest.fixture(scope="module")
 def token():
     return generate_jwt("498592a0-5408-487d-aa0d-1cd6be2d0853")
+
+@pytest.fixture
+def new_user_id(client):
+    register_payload = {"first_name": "NEW", "last_name":"REGISTER", "email": "newregister@gmail.com","password":"mypassword"}
+    res = client.post("/api/v1/register", json = register_payload)
+    yield res
+    
+@pytest.fixture
+def setup_header(client, new_user_id):
+    data = new_user_id.json()
+    header = {"Authorization":f"Bearer {data['token']}"}
+    yield header
+    client.delete("/api/v1/users", headers=header)
+    
+
+    
