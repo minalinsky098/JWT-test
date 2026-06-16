cd4a56d5c91916a36269e2e55a36f92c11965b77
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-06-16 10:58:46 +0800

    feat: added update username tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,6 +2,7 @@ import pytest
 from unittest.mock import patch, AsyncMock
 from fastapi.testclient import TestClient
 from backend.exceptions import DatabaseError
+from backend.auth import decode_jwt_user_id, SECRET, ALGORITHM
 
 
 async def test_get_all_users(client):
@@ -56,3 +57,31 @@ async def test_login_database_error(client):
         assert res.status_code == 500
         assert res.json()["detail"] == "Internal Server Error"
   
+async def test_register_delete_duplicate(client):
+    register_payload = {"first_name": "NEW", "last_name":"REGISTER", "email": "newregister@gmail.com","password":"mypassword"}
+    res = client.post("/api/v1/register", json = register_payload)
+    data = res.json()
+    header = {"Authorization":f"Bearer {data["token"]}"}
+    assert res.status_code == 201
+    res2 = client.post("/api/v1/register", json = register_payload)
+    assert res2.status_code == 409
+    delete_res = client.delete("/api/v1/users", headers=header)
+    assert delete_res.status_code == 200
+    
+async def test_register_create_new_user_error(client):
+    payload = {"first_name": "RYC", "last_name":"ALB", "email": "alboloter@gmail.com","password":"mypassword"}
+    with patch("backend.JWTserver.create_new_user", new_callable=AsyncMock) as mock:
+        mock.side_effect = Exception
+        res = client.post("/api/v1/register", json = payload)
+        assert res.status_code == 409
+        
+async def test_update_user_name_happy(client, setup_header):
+    update_payload = {"first_name": "Newfirstname", "last_name": "newlastname"}
+    res = client.put("/api/v1/users", json = update_payload,headers=setup_header)
+    assert res.status_code == 200
+    
+async def test_update_user_not_found(client, setup_header):
+    update_payload = {"first_name": "Newfirstname", "last_name": "newlastname"}
+    client.delete("/api/v1/users", headers=setup_header)
+    res = client.put("/api/v1/users", json = update_payload,headers=setup_header)
+    assert res.status_code == 404
