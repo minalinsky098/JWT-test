92e54ec44c0f583efe6079ec77ef058aab9bf9c4
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-06-19 14:58:12 +0800

    feat: added api call for fetching cats with a default value of 45

--- backend/api.py
+++ backend/api.py
@@ -14,16 +14,18 @@ load_dotenv()
 CAT_API = os.getenv("CATAPI")
 BASE_URL = os.getenv("CATURL")
 
-async def fetch_cats(limit: int):
+async def fetch_cats(limit: int = 45):
     SEARCH_URL = f"{BASE_URL}/images/search"
     header = {"x-api-key":CAT_API}
     try:
         async with AsyncClient() as client:
-            res = await client.get(SEARCH_URL, headers=header, params={"limit":limit})
+            res = await client.get(SEARCH_URL, headers=header, params={"limit":limit, "has_breeds":1})
+            res.raise_for_status()
             data = res.json()
-            for _ in data:
-                print(_["url"])
+            normalized_fetch = []
+            for cat in data:
+                normalized_fetch.append({"url":cat["url"], "breed":cat["breeds"][0]["name"], "description":cat["breeds"][0]["description"]})
+            return normalized_fetch
     except Exception as e:
-        logger.exception(str(e))
+        raise
 
-asyncio.run(fetch_cats(5))
