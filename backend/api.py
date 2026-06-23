a1533a3fa979c11621087d3f1d5cefe466f34be8
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-06-23 13:11:58 +0800

    refactor: removed some useless codes

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,7 @@
-from httpx import AsyncClient
+import httpx 
 import os 
-import asyncio
 from dotenv import load_dotenv
+from exceptions import CatAPIError
 
 load_dotenv()
 CAT_API = os.getenv("CATAPI")
@@ -11,14 +11,17 @@ async def fetch_cats(limit: int = 45):
     SEARCH_URL = f"{BASE_URL}/images/search"
     header = {"x-api-key":CAT_API}
     try:
-        async with AsyncClient() as client:
+        async with httpx.AsyncClient() as client:
             res = await client.get(SEARCH_URL, headers=header, params={"limit":limit, "has_breeds":1})
+            print(res)
             res.raise_for_status()
             data = res.json()
             normalized_fetch = []
             for cat in data:
                 normalized_fetch.append({"url":cat["url"], "breed":cat["breeds"][0]["name"], "description":cat["breeds"][0]["description"]})
             return normalized_fetch
-    except Exception:
-        raise
+    except (httpx.HTTPStatusError, httpx.RequestError) as e:
+        raise CatAPIError from e
+    except Exception as e:
+        raise 
 
